aa2d7c740f1695423b0f2d8987c23b044074c234
vaghela <vaghelakaranraj54@gmail.com>
2023-12-23 14:40:25 +0530

    intial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fcbd088e-9455-42b7-baab-39dd21a6932e';
+export const MODAL_CLOSE_SEC = 2.5;
